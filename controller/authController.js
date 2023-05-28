862726bd48b9ad9ef5108a02053ee57f65ff9986
ivansiuzzzzz <ivan.siu@icloud.com>
2023-05-28 22:48:00 +0800

    signup with jsonwebtoken

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,17 @@
 const User = require("../models/userModel");
+const jwt = require('jsonwebtoken')
+
 
 exports.signup = async(req, res, next)=> {
     const newUser = await User.create(req.body)
 
+    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+
     res.status(201).json({
         status: "success",
+        token,
         data: {
             user: newUser
         }
