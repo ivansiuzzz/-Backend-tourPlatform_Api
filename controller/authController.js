89386954f087188506cb297c73c77478803b8ee3
ivansiuzzzzz <ivan.siu@icloud.com>
2023-05-28 23:12:59 +0800

    login jwt

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,16 @@
 const User = require("../models/userModel");
 const jwt = require('jsonwebtoken')
 
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    });
+  };
 
 exports.signup = async(req, res, next)=> {
     const newUser = await User.create(req.body)
 
-    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
+    const token = signToken(newUser._id)
 
     res.status(201).json({
         status: "success",
@@ -16,5 +19,33 @@ exports.signup = async(req, res, next)=> {
             user: newUser
         }
     })
-
 }
+
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      return next(
+        res.status(400).json({
+        status: 'fail',
+        message: "email or password do not provide"
+        })
+    );
+    }
+
+    const user = await User.findOne({ email }).select('+password'); // only need passport    
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next( res.status(401).json({
+        status: 'fail',
+        message: "email or password do not correct"
+        }));
+    }
+  
+    const token = signToken(user._id)
+    res.status(200).json({
+        status: 'success',
+        token
+    })
+    // createSendToken(user, 200, res);
+  };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const authController = require('../controller/authController')
 
 router.route('/signup').post(authController.signup)
+router.route('/login').post(authController.login)
 
 module.exports = router;
