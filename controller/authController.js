2519a6340467fa02728ce593e229d922a77745cc
ivansiuzzzzz <ivan.siu@icloud.com>
2023-05-29 00:13:58 +0800

    add verification token

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require('jsonwebtoken')
+const { promisify } = require('util');
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -49,3 +50,56 @@ exports.login = async (req, res, next) => {
     })
     // createSendToken(user, 200, res);
   };
+
+  exports.protect = async (req, res, next) => {
+    // 1) Getting token and check of it's there
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    }
+  
+    console.log(token);
+    if (!token) {
+      return next( res.status(403).json({
+        status: 'fail',
+        message: "Please login first, you don't have access right"
+        }))
+    }
+  
+    // 2) Verification token
+        await promisify(jwt.verify)(token, process.env.JWT_SECRET).catch((err)=> next(res.status(403).json({
+            status: 'fail',
+            message: err
+            })));
+   
+            next();
+
+    // // 3) Check if user still exists
+    // const currentUser = await User.findById(decoded.id);
+    // if (!currentUser) {
+    //   return next(
+    //     new AppError(
+    //       'The user belonging to this token does no longer exist.',
+    //       401
+    //     )
+    //   );
+    // }
+  
+    // 4) Check if user changed password after the token was issued
+    // if (currentUser.changedPasswordAfter(decoded.iat)) {
+    //   return next(
+    //     new AppError('User recently changed password! Please log in again.', 401)
+    //   );  
+    // }
+
+    //   // GRANT ACCESS TO PROTECTED ROUTE
+    //   req.user = currentUser;
+    //   res.locals.user = currentUser;
+    //   next();
+    };
+    
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const tourController = require('./../controller/tourController')
+const authController = require('../controller/authController')
 
-
-router.route('/').get(tourController.getAllTours).post(tourController.checkBody, tourController.createTour)
+router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.checkBody, tourController.createTour)
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour)
 
 module.exports = router;
